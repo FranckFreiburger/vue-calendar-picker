d76c1540d128241fba66ef32421eb2af9b518ee6
Franck Freiburger <franck.freiburger@gmail.com>
2017-10-31 14:58:31 +0100

    - add dataset polyfill

--- src/findDataAttr.js
+++ src/findDataAttr.js
@@ -1,9 +1,42 @@
+function camelize(str) {
+
+	return str.replace(/-./g, function(str) { return str.charAt(1).toUpperCase() });	
+}
+
+var dataset = function(elt) {
+
+	if ( 'dataset' in HTMLElement.prototype ) {
+
+		dataset = function(elt) {
+			
+			return elt.dataset;
+		}
+	} else {
+
+		dataset = function(elt) {
+
+			var data = {};
+			for ( var i = 0; i < elt.attributes.length; ++i ) {
+
+				var attribute = elt.attributes[i];
+				if ( attribute.name.substr(0, 5) === 'data-' )
+					data[camelize(attribute.name.substr(5))] = attribute.value;
+			}
+			return data;
+		}
+	}
+	return dataset(elt);
+}
+
 export default function(elt, rootElt) {
 	
 	var dataAttrMap = {};
 	for ( ; elt !== rootElt && elt !== null; elt = elt.parentNode )
-		if ( elt.nodeType === 1 )
-			for ( var propName in elt.dataset )
-				dataAttrMap[propName] = elt.dataset[propName];
+		if ( elt.nodeType === 1 ) {
+
+			var data = dataset(elt);
+			for ( var propName in data )
+				dataAttrMap[propName] = data[propName];
+		}
 	return dataAttrMap;
 }
